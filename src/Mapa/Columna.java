341eddc4eec3262a79767b3bd771055c0a5058ad
caropistillo <carup8@hotmail.com>
2018-11-17 16:37:19 -0300

    Create Columna.java

---
+++ src/Mapa/Columna.java
@@ -0,0 +1,14 @@
+package Mapa;
+
+import java.util.ArrayList;
+
+public class Columna extends ConjuntoDeCasilleros
+{
+
+    public Columna(int tamanio)
+    {
+        this.tamanio=tamanio;
+
+    }
+
+}
