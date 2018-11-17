8871aa6add782f672550ceed7924d90c39673853
caropistillo <carup8@hotmail.com>
2018-11-17 16:37:40 -0300

    Create ConjuntoDeCasilleros.java

---
+++ src/Mapa/ConjuntoDeCasilleros.java
@@ -0,0 +1,38 @@
+package Mapa;
+
+import java.util.ArrayList;
+
+public class ConjuntoDeCasilleros
+{
+    private ArrayList<Casillero> lista = new ArrayList<Casillero>();
+    protected int tamanio;
+
+
+    public void referenciarCasillero(Casillero casillero)
+    {
+        this.lista.add(casillero);
+        return;
+    }
+
+
+
+    public int obtenerTamanio()
+    {
+        return this.tamanio;
+    }
+
+    public boolean contiene(int numero)
+    {
+        return true;
+    }
+
+    public Casillero obtenerCasillero(int posicion)
+    {
+        return lista.get(posicion);
+    }
+    public int obtenerElemento(int posicion)
+    {
+        return this.obtenerCasillero(posicion).obtenerElemento();
+    }
+
+}
