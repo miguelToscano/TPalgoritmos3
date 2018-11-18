83c2ea728e1556ad4e710ac5c5d046c6af9465f2
caropistillo <carup8@hotmail.com>
2018-11-18 18:15:17 -0300

    ..

--- src/edificios/Edificio.java
+++ src/edificios/Edificio.java
@@ -1,13 +1,16 @@
 package edificios;
 
 import unidades.Entidad;
+import mapa.Caja;
 
 public abstract class Edificio extends Entidad {
 
-	
+	protected Caja cajaOcupada;
+
 	protected int velocidadReparacion;
 	
-	public void reparar() {
+	public void reparar()
+	{
 		vida += velocidadReparacion;
 	}
 
