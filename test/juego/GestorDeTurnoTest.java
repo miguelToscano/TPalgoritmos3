fe79de471345aedc4ad147ab511da9635aab347b
coniviere <43079502+coniviere@users.noreply.github.com>
2018-11-26 19:36:29 -0300

    Create GestorDeTurnoTest.java

---
+++ test/juego/GestorDeTurnoTest.java
@@ -0,0 +1,39 @@
+package juego;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+import juego.*;
+import mapa.Mapa;
+import unidades.Aldeano;
+
+public class GestorDeTurnosTest {
+	
+	private Jugador jugadorMockA= Mockito.mock(Jugador.class);
+	private Jugador jugadorMockB= Mockito.mock(Jugador.class);
+	private GestorDeTurno gestorMock= Mockito.mock(GestorDeTurno.class);
+	private int turno=1;
+	
+	GestorDeTurno gestor= new GestorDeTurno(jugadorMockA, jugadorMockB, turno);
+	
+	  @Test
+	  public void finalizadoElTurnoSeCambiaDeJugador() {
+		  
+		  Jugador primerJugador = this.gestor.obtenerPrimerJugador();
+		  this.gestor.finalizarTurno();
+		  
+		  Assert.assertTrue(this.gestor.obtenerJugadorSiguiente()!=primerJugador);
+	  }
+	  
+		
+	  @Test
+	  public void finalizadoElTurnoSeIncrementaNumeroTurno() {
+		  
+		  int numeroTurnoAlPpio = this.gestor.obtenerTurnoActual();
+		  this.gestor.finalizarTurno();
+		  
+		  Assert.assertTrue(numeroTurnoAlPpio<this.gestor.obtenerTurnoActual());
+	  }
+}
