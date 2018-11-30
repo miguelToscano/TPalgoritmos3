41b1e6117843d418b1382504adb7e9e606027cd1
coniviere <43079502+coniviere@users.noreply.github.com>
2018-11-29 22:57:37 -0300

    Update Jugador.java
    perdioLaPartida, habilitar, deshabilitar

--- src/juego/Jugador.java
+++ src/juego/Jugador.java
@@ -2,17 +2,16 @@ package juego;
 import edificios.*;
 import excepciones.superaLimitePoblacional;
 import unidades.*;
-
 import java.util.ArrayList;
 
-//Cada jugador comienza con una plaza central y un castillo. No es posible construir un castillo. Sólo
-// existirá el que es asignado al empezar la partida. Sí es posible construir plazas centrales y cuarteles sin límite.
+//Cada jugador comienza con una plaza central y un castillo. No es posible construir un castillo. SÃ³lo
+// existirÃ¡ el que es asignado al empezar la partida. SÃ­ es posible construir plazas centrales y cuarteles sin lÃ­mite.
 //
 //Cada jugador comienza la partida con 3 aldeanos y 100 de oro
 //
 //Los jugadores inician en extremos opuestos del mapa.
 //
-//El límite de población es 50 (para cada jugador, es decir, 100 en total). Cada unidad del jugador ocupa 1 lugar de población. Los edificios no ocupan lugar de población.
+//El lÃ­mite de poblaciÃ³n es 50 (para cada jugador, es decir, 100 en total). Cada unidad del jugador ocupa 1 lugar de poblaciÃ³n. Los edificios no ocupan lugar de poblaciÃ³n.
 
 
 public class Jugador
@@ -24,10 +23,11 @@ public class Jugador
     private ArrayList<ArmaDeAsedio> armas = new ArrayList<ArmaDeAsedio>();
     private ArrayList<Arquero> arqueros = new ArrayList<Arquero>();
     private ArrayList<Espadachin> espadachines = new ArrayList<Espadachin>();
-
+    private boolean habilitado=false;
+    
     final static int ORO_INICIAL = 100;
     final static int LIMITE_POBLACION = 50;
-
+    
     protected int oro;
     protected int poblacion;
 
@@ -35,6 +35,7 @@ public class Jugador
 
         this.oro = ORO_INICIAL;
         this.poblacion = 0;
+        this.habilitado=false;
 
     }
 
@@ -93,5 +94,20 @@ public class Jugador
 
         this.oro += oro;
     }
-
+    
+    public void habilitar() {
+    	this.habilitado=true;
+    }
+    
+    public void deshabilitar() {
+    	this.habilitado=false;
+    }
+    
+    public boolean esTuTurno() {
+    	return (this.habilitado);
+    }
+    
+    public boolean perdioLaPartida() {
+    	return (this.getCastillo().destruidoTotalmente());
+    }
 }
